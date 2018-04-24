1f2dd7e6fd8b4c6915b96cfea304212a3979af6d
Shaun <tbevan@gmail.com>
2018-04-24 04:22:04 -0400

    Update images

---
+++ BaseAppV2/Views/SubViews/TravelPass/CameraView.swift
@@ -0,0 +1,66 @@
+//
+//  CameraView.swift
+//  BaseAppV2
+//
+//  Created by Shaun Bevan on 4/24/18.
+//  Copyright © 2018 SilverLogic. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+import UIKit
+
+final class CameraView: UIView {
+
+    private lazy var videoDataOutput: AVCaptureVideoDataOutput = {
+        let v = AVCaptureVideoDataOutput()
+        v.alwaysDiscardsLateVideoFrames = true
+        v.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
+        v.connection(with: .video)?.isEnabled = true
+        return v
+    }()
+
+    private let videoDataOutputQueue: DispatchQueue = DispatchQueue(label: "JKVideoDataOutputQueue")
+    private lazy var previewLayer: AVCaptureVideoPreviewLayer = {
+        let l = AVCaptureVideoPreviewLayer(session: session)
+        l.videoGravity = .resizeAspect
+        return l
+    }()
+
+    private let captureDevice: AVCaptureDevice? = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back)
+    private lazy var session: AVCaptureSession = {
+        let s = AVCaptureSession()
+        s.sessionPreset = .vga640x480
+        return s
+    }()
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        contentMode = .scaleAspectFit
+        beginSession()
+    }
+
+    private func beginSession() {
+        do {
+            guard let captureDevice = captureDevice else {
+                fatalError("Camera doesn't work on the simulator! You have to test this on an actual device!")
+            }
+            let deviceInput = try AVCaptureDeviceInput(device: captureDevice)
+            if session.canAddInput(deviceInput) {
+                session.addInput(deviceInput)
+            }
+
+            if session.canAddOutput(videoDataOutput) {
+                session.addOutput(videoDataOutput)
+            }
+            layer.masksToBounds = true
+            layer.addSublayer(previewLayer)
+            previewLayer.frame = UIScreen.main.bounds
+            session.startRunning()
+        } catch let error {
+            debugPrint("\(self.self): \(#function) line: \(#line).  \(error.localizedDescription)")
+        }
+    }
+}
+
+extension CameraView: AVCaptureVideoDataOutputSampleBufferDelegate {}
